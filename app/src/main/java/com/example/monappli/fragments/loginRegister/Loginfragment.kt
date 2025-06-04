2923013c34ce886fd4c301c58da20329e5fd6d9f
IKRAMELHADI <152861932+IKRAMELHADI@users.noreply.github.com>
2025-06-04 09:39:22 +0200

    Suppression prblms

--- app/src/main/java/com/example/monappli/fragments/loginRegister/Loginfragment.kt
+++
@@ -1,5 +0,0 @@
-package com.example.monappli.fragments.loginRegister
-import androidx.fragment.app.Fragment
-import com.example.monappli.R
-class Loginfragment : Fragment(R.layout.fragment_login) {
-}
